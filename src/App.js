e76c689e8b5b9ef6a7bb9b2f3494fd42c296134a
Santiago <smontoyaorozco@gmail.com>
2022-01-10 14:30:56 -0500

    See you! msg

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          Bye There!
+          Bye There! See you!
         </p>
         <a
           className="App-link"
